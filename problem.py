99cc3173765ec40bfa714816b8410e44fb647a89
Gustavo <105085153+rystei@users.noreply.github.com>
2023-05-22 15:44:23 -0300

    correção
    faltava (:) no bloco if para indicar seu inicio.

---
+++ problem.py
@@ -0,0 +1,18 @@
+  #Problema:
+
+    # minha_idade = 26
+    # idade_namorado = 25
+    # if(minha_idade == idade_namorado)
+    # print('temos idades iguais')
+    # else:
+    # print('temos idades diferentes')
+#--------------------------------------------------------------
+
+#código arrumado:
+
+minha_idade = 26
+idade_namorado = 26
+if(minha_idade == idade_namorado):
+    print('temos idades iguais')
+else:
+    print('temos idades diferentes')
