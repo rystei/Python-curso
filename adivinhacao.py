0dd88867364e7e2fb233f25b0fca874258505ef4
Gustavo <105085153+rystei@users.noreply.github.com>
2023-06-06 12:00:39 -0300

    Update adivinhacao.py
    Atualização do código com incremento da função (while) para permitir mais tentativas no jogo

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,23 +3,26 @@ print("Bem vindo ao jogo de adivinhação !")
 print("**********************************")
 
 numero_secreto = 30
+total_de_tentativas = 3
+rodada = 1
 
-chute_str = input("digite um numero: ")
+while (rodada <= total_de_tentativas):
+    print("tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("digite um numero: ")
+    print("você digitou, ", chute_str)
+    chute = int(chute_str)
 
-print("você digitou, ", chute_str)
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-chute = int(chute_str)
+    if acertou:
+        print("você acertou o numero!")
+    else:
+        if (maior):
+            print("você errou o numero! :(, seu chute foi maior que o numero secreto ")
+        elif (menor):
+            print("você errou o numero! :(, seu chute foi menor que o numero secreto ")
 
-acertou = chute == numero_secreto
-maior   = chute > numero_secreto
-menor   = chute < numero_secreto
-
-if acertou:
-    print("você acertou o numero!")
-else:
-    if (maior):
-        print("você errou o numero! :(, seu chute foi maior que o numero secreto ")
-    if (menor):
-        print("você errou o numero! :(, seu chute foi menor que o numero secreto ")
-
+    rodada = rodada + 1
 print("Acabou o jogo!")
